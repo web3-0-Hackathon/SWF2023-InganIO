6e3173890436ba161d1a18710fead18cfa524d64
seinShin <tlstpdls13@gmail.com>
2023-08-01 09:41:57 +0900

    # 사용자 지갑 출금 신청 , 자식 주소 저장 api # nft 출금 신청 api

--- src/main/java/inganio/demo/User/Service/UserService.java
+++ src/main/java/inganio/demo/User/Service/UserService.java
@@ -5,29 +5,45 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import inganio.demo.Common.commonUtil;
 import inganio.demo.User.Mapper.UserMapper;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 @Service
 public class UserService {
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private UserMapper userMapper;
 	
-	public String userWithdrawals(HashMap<String, String> paramMap) throws SQLException, IOException {
+	/**************************************************
+	* @MethodName : userWithdrawals
+	* @Description: 사용자 지갑 출금 신청 - 자식 주소 저장
+	* @return String
+	* @throws ParseException 
+	* @Author : se-in shin
+	**************************************************/
+	public String userWithdrawals(HashMap<String, String> paramMap) throws SQLException, IOException, ParseException {
 		String uuid = null;
 		
 		// 부여받은 자식 주소 정보
 		String requestId = paramMap.get("requestId");
 		String amount = paramMap.get("amount");
 		String childAddress = paramMap.get("childAddress");
+		
 		//유저 1 정보 조회
 		Map<String, Object> userMap = userMapper.getUserInfo();
 //		String rsaKey = (String) userMap.get("rsaKey");
@@ -43,15 +59,16 @@ public class UserService {
 			rst=userMapper.childAddress(userMap);
 		} catch (Exception e) {
 			// TODO: handle exception
+			logger.debug("error: "+e);
 		}
 		
 		//자식주소로 출금 신청
 		OkHttpClient client = new OkHttpClient();
 
 		MediaType mediaType = MediaType.parse("application/json");
-		RequestBody body = RequestBody.create(mediaType, "{\"symbol\":\"ETH\",\"requestId\":\""+requestId+"\",\"amount\":\""+amount+"\",\"senderAddress\":\""+childAddress+"\",\"receiverAddress\":\""+childAddress+"\"}");
+		RequestBody body = RequestBody.create(mediaType, "{\"symbol\":\"ETH\",\"requestId\":\""+requestId+"\",\"amount\":\""+amount+"\",\"senderAddress\":\""+userAddress+"\",\"receiverAddress\":\""+childAddress+"\"}");
 		Request request = new Request.Builder()
-		  .url("https://octet-api.blockchainapi.io/2.0/wallets/"+walletNum+"/withdrawals")
+		  .url("https://tetco-api.blockchainapi.io/2.0/wallets/"+walletNum+"/withdrawals")
 		  .post(body)
 		  .addHeader("accept", "application/json")
 		  .addHeader("Authorization", "Bearer "+apiToken)
@@ -60,8 +77,105 @@ public class UserService {
 
 		Response response = client.newCall(request).execute();	
 		uuid = response.body().toString();
+		
+		
+		//출금 상태 조회 - 출금 신청 정보 조회 api
+		String tranStatus = null;
+		if(!uuid.isEmpty()) {
+			OkHttpClient client2 = new OkHttpClient();
+
+			Request request2 = new Request.Builder()
+			  .url("https://tetco-api.blockchainapi.io/2.0/wallets/"+walletNum+"/withdrawals/"+uuid)
+			  .get()
+			  .addHeader("accept", "application/json")
+			  .addHeader("Authorization", "Bearer ")
+			  .build();
+
+			Response response2 = client2.newCall(request2).execute();
+			ResponseBody responseBody = response2.body();
+			
+			JSONParser paser = new JSONParser();
+	        JSONObject tranInfo = (JSONObject) paser.parse(responseBody.string());
+	        
+	        tranStatus = (String) tranInfo.get("state");
+	        System.out.println("트랜잭션 상태 ---- "+ tranStatus);
+		}
+		
+		return tranStatus;
+	}
 	
-		return uuid;
+	/**************************************************
+	* @MethodName : NftWithdrawalsOffer
+	* @Description: 사용자가 호스트에게 NFT 출금 신청 후 트랜잭션 등록
+	* @return String
+	* @throws IOException 
+	* @throws SQLException 
+	* @throws ParseException 
+	* @Author : se-in shin
+	**************************************************/
+	public String NftWithdrawalsOffer(HashMap<String, String> paramMap) throws IOException, SQLException, ParseException {
+		// 프론트에서 넘겨준 유저 + 행사 정보
+		String requestId = paramMap.get("requestId");
+		String amount = paramMap.get("amount");
+		String childAddress = paramMap.get("childAddress");
+		String contractAddress = paramMap.get("contractAddress");
+				
+		//유저 1 정보 조회
+		Map<String, Object> userMap = userMapper.getUserInfo();
+		String rsaKey = (String) userMap.get("rsaKey");
+		String apiToken =(String) userMap.get("apiToken");
+		String walletNum =(String) userMap.get("walletNum");
+		String userAddress = (String) userMap.get("userAddress");
+		
+		//nft 출금 신청
+		OkHttpClient client = new OkHttpClient();
+
+		MediaType mediaType = MediaType.parse("application/json");
+		RequestBody body = RequestBody.create(mediaType, "{\"requestId\":\""+requestId+"\",\"encryptedUserKey\":\""+rsaKey+"\",\"contractAddress\":\""+contractAddress+"\",\"tokenId\":\""+requestId+"\",\"receiverAddress\":\""+childAddress+"\",\"amount\":1,\"gasPrice\":\""+amount+"}");
+		Request request = new Request.Builder()
+		  .url("https://tetco-api.blockchainapi.io/2.0/wallets/"+walletNum+"/nfts/main-address/withdrawals")
+		  .post(body)
+		  .addHeader("accept", "application/json")
+		  .addHeader("Authorization", "Bearer "+apiToken)
+		  .addHeader("content-type", "application/json")
+		  .build();
+
+		Response response = client.newCall(request).execute();
+		
+		String uuid = response.body().toString();
+		
+		
+		String status = "";
+		//uuid값이 발행되면 nft 출금 신청 정보 조회 후 디비 등록
+		if (!uuid.isEmpty()){
+			OkHttpClient client2 = new OkHttpClient();
+
+			Request request2 = new Request.Builder()
+			  .url("https://tetco-api.blockchainapi.io/2.0/wallets/"+walletNum+"/nfts/withdrawals/"+uuid)
+			  .get()
+			  .addHeader("accept", "application/json")
+			  .addHeader("Authorization", "Bearer "+apiToken)
+			  .build();
+
+			Response response2 = client2.newCall(request2).execute();
+			
+			
+			//저장 보류 일단 상태값만 반환
+			ResponseBody responseBody = response2.body();
+			JSONParser paser = new JSONParser();
+	        JSONObject nftTxn = (JSONObject) paser.parse(responseBody.string());
+	        status = (String) nftTxn.get("status");
+			
+			
+//			ResponseBody responseBody = response2.body();
+//			JSONParser paser = new JSONParser();
+//	        JSONObject obj = (JSONObject) paser.parse(responseBody.string());
+//			HashMap<String, String> nftMap = (HashMap<String, String>) commonUtil.getMapFromJsonObject(obj).get("nft");
+		}else {
+			logger.debug("nft 출금 신청 에러");
+		}
+		
+		return "NFT_"+status;
 	}
 
 }
--- src/main/java/inganio/demo/User/UserController.java
+++ src/main/java/inganio/demo/User/UserController.java
@@ -1,10 +1,12 @@
 package inganio.demo.User;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import inganio.demo.Common.commonUtil;
 import inganio.demo.User.Mapper.UserMapper;
 import inganio.demo.User.Service.UserService;
 
@@ -31,26 +32,6 @@ public class UserController {
 	
 	@Autowired
 	private UserService userService;
-//	
-//	@GetMapping({"/userList/{title}"})
-//	public Object userList(HttpServletRequest request) throws SQLException {
-//		CamelMap paramMap = HttpUtil.getRequestCamelMap(request);
-//		List<CamelMap> rstList = null;
-//		ModelAndView mv = new ModelAndView("jsonView");
-//		
-//		try {
-//			rstList = userMapper.userList(paramMap);
-//			System.out.println(rstList);
-//		} catch (Exception e) {
-//			// TODO: handle exception
-//		}
-//		
-//		mv.addObject("rstList", rstList);
-//		
-//		return mv;
-//		
-//	} 
-//	
 	//headers - get 성공
 	//.join 성공
 	//url 뒤에 바로 붙음
@@ -59,12 +40,10 @@ public class UserController {
 		return userMapper.userList(pass);
 	}
 	
-	
 	//headers - get 성공
 	//.join 성공
 	@GetMapping({"/test"})
 	public List<Map<String, Object>> test(@RequestParam HashMap<String,String> paramMap) throws SQLException {
-//		String pass = paramMap.get("pass");
 		System.out.println(paramMap);
 		List<Map<String, Object>> rst = userMapper.test(paramMap);
 		return rst;
@@ -94,36 +73,32 @@ public class UserController {
 		return userInfo;
 	}  
 	
-	//?로 붙음
-//	@GetMapping({"/test"})
-//	public Map<String, String>  user(@RequestParam Map<String, String> paramMap) {
-//		logger.debug("파라미터 찍업", paramMap.get("name"));
-//		/* HashMap<String, String> rstMap = new HashMap<String, String>(); */
-//		
-//		/* JSONObject jsonObject = new JSONObject(rstMap); */
-//		  
-//		return userMapper.user(paramMap);
-//		
-//	}
-	
 	/**************************************************
 	* @MethodName : userWithdrawals
-	* @Description: 유저 지갑 출금 신청
-	* @return int
+	* @Description: 사용자 지갑 출금 신청 (자식주소 생성 후 출금상태 전송)
+	* @return String
+	* @throws ParseException 
+	* @throws SQLException 
+	* @throws IOException 
 	* @Author : se-in shin
 	**************************************************/
 	@PostMapping({"/userWithdrawals"})
-	public int userWithdrawals(@RequestBody HashMap<String, String> paramMap){
-		int status = commonUtil.StatusCode.INTERNAL_SERVER_ERROR;
-		String uuid = null;
+	public String userWithdrawals(@RequestBody HashMap<String, String> paramMap) throws IOException, SQLException, ParseException{
+		String tranStatus = null;
 		try {
-			uuid = userService.userWithdrawals(paramMap);
-			
+			tranStatus = userService.userWithdrawals(paramMap);
 		} catch (Exception e) {
 			// TODO: handle exception
 			logger.debug("error:"+ e);
 		}
-		return status = 0;
+		
+		// 출금이 완료되었다면 nft 출금 신청
+		if (tranStatus.equals("SENT")){
+			tranStatus = userService.NftWithdrawalsOffer(paramMap);
+		}
+		
+		//아니면 에러코드 반환.
+		return tranStatus;
 	} 
 	
 
