5211691daa049f8fd7c8a9a5ecbf51d92caff261
thsamajiki <ant1102@naver.com>
2023-08-01 16:02:35 +0900

    호스트가 등록한 행사의 컨트랙트 주소와 토큰 아이디 조회 API 추가

---
+++ app/src/main/java/web3_hackathon/humanio/data/api/human_io/GetNftContractItemInfoApi.kt
@@ -0,0 +1,19 @@
+package web3_hackathon.humanio.data.api.human_io
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GetNftContractItemInfoApi {
+    @GET("user/nftContractItemInfo")
+    fun getNftContractItemInfo(
+    @Query("hostId") hostId: Int = 1,
+    @Query("eventId") eventId: Int = 1,
+    ): Call<ContractItemInfoResponse>
+}
+
+data class ContractItemInfoResponse(
+    @SerializedName("contractAddress") val contractAddress: String,
+    @SerializedName("tokenId") val tokenId: String
+)
